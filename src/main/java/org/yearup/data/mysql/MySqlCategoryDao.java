454b04eb23f4fabaab1daa2371db93aa7d2b661b
Amena Nazari <nazara@jevs.org>
2025-06-23 12:43:13 -0400

    add the create and update categorys

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -96,9 +96,24 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
 
     @Override
     public void update(int categoryId, Category category)
-    {
-        // update category
+    { String sql = "UPDATE categories SET name = ?, description = ? WHERE category-id = ?";
+
+        try (Connection connection = getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql))
+        {
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+            statement.setInt(3, categoryId);
+            statement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
+
+    // update category
+
 
     @Override
     public void delete(int categoryId)
